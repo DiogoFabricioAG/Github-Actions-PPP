eb761f46781b1e323cc2d02aeef959dcc5c72190
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-02-19 11:48:08 -0500

    automatizar

---
+++ automatization.py
@@ -0,0 +1,7 @@
+def log_current_datetime():
+    from datetime import datetime
+    current_datetime = datetime.now()
+    print(f"Current date and time: {current_datetime}")
+
+if __name__ == "__main__":
+    log_current_datetime()
