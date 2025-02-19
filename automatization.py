0e88613aa545932df631293a97e6490ce50524ae
DiogoFabricioAG <diogo.abregu.g@uni.pe>
2025-02-19 12:16:38 -0500

    agregando datos

--- automatization.py
+++ automatization.py
@@ -1,7 +1,8 @@
 def log_current_datetime():
     from datetime import datetime
     current_datetime = datetime.now()
-    print(f"Current date and time: {current_datetime}")
+    with open("dia_actual.txt", "w") as file:
+        file.write(f"Current date and time: {current_datetime}")
 
 if __name__ == "__main__":
     log_current_datetime()
